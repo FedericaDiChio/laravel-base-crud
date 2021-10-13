faa0a8bdbfc0834d53927cfe5c3e0faf346bf0a4
FedericaDiChio <federica.dichio7@gmail.com>
2021-10-13 16:20:12 +0200

    aggiunto update e form comune a create & edit

--- resources/views/includes/comics/form.blade.php
+++ resources/views/includes/comics/form.blade.php
@@ -1,39 +1,45 @@
-<form method="POST" action="{{route('comics.store')}}">
+@if($comic->exists)
+    <form method="POST" action="{{route('comics.update', $comic->id)}}">
+        @method('PATCH')
+@else 
+    <form method="POST" action="{{route('comics.store')}}">
+@endif        
     @csrf
-    <div>
-        <label for="title">Titolo fumetto</label>
-        <input type="text" id="title" name="title">
-    </div>
-    <div>
-        <label for="price">Prezzo</label>
-        <input type="number" min="0" max="100" step="0.01" id="price" name="price">
-    </div>
-    <div>
-        <label for="series">Serie</label>
-        <input type="text" id="series" name="series">
-    </div>
-    <div>
-        <label for="type">Tipologia</label>
-        <select name="type" id="type">
-            <option>Comic Book</option>
-            <option>Graphic Novel</option>
-        </select>
-        
-    </div>
-    <div>
-        <label for="sale_date">Data uscita</label>
-        <input type="date" id="sale_date" name="sale_date">
-    </div>
-    <div>
-        <label for="thumb">Inserisci un'immagine</label>
-        <input type="text" id="thumb" name="thumb">
-    </div>
-    <div>
-        <label for="decription">Descrizione</label>
-        <textarea  id="description" name="description" cols="30" rows="10"></textarea>
-        
-    </div>
-    <div>
-        <button type="submit">Inserisci fumetto</button>
-    </div>
+        <div>
+            <label for="title">Titolo fumetto</label>
+            <input type="text" id="title" name="title" value="{{ $comic->title}}">
+        </div>
+        <div>
+            <label for="price">Prezzo</label>
+            <input type="number" min="0" max="100" step="0.01" id="price" name="price" value="{{ $comic->price}}">
+        </div>
+        <div>
+            <label for="series">Serie</label>
+            <input type="text" id="series" name="series" value="{{ $comic->series}}">
+        </div>
+        <div>
+            <label for="type">Tipologia</label>
+            <select name="type" id="type">
+                <option @if ($comic->type === 'Comic Book') selected @endif>Comic Book</option>
+                <option @if ($comic->type === 'Graphic Novel') selected @endif>Graphic Novel</option>
+            </select>
+            
+        </div>
+        <div>
+            <label for="sale_date">Data uscita</label>
+            <input type="date" id="sale_date" name="sale_date" value="{{ $comic->sale_date}}">
+        </div>
+        <div>
+            <label for="thumb">Inserisci un'immagine</label>
+            <input type="text" id="thumb" name="thumb" value="{{ $comic->thumb}}">
+        </div>
+        <div>
+            <label for="decription">Descrizione</label>
+            <textarea  id="description" name="description" cols="30" rows="10">{{ $comic->description}}</textarea>
+            
+        </div>
+        <div>
+            <button type="submit">Inserisci fumetto</button>
+        </div>
+    </form>
 </form>
